2f90b03141028208edd83a44f23e7b0196dfd4dd
Doni Sanjaya <donis293@gmail.com>
2023-12-12 23:29:44 +0700

    Update VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -23,87 +23,150 @@ const VideoCard = ({
 		id: { videoId },
 		snippet,
 	},
-}) => (
-	<Card
-		sx={{
-			width: { xs: "100%", sm: "358px", md: "425px" },
-			boxShadow: "none",
-			borderRadius: 2,
-			border: "1px solid gray",
-		}}
-	>
-		<Grid container spacing={2}>
-			<Grid item xs={6} sm={6}>
-				<Stack>
-					<Link
-						to={
-							videoId
-								? `/video/${videoId}`
-								: `/video/cV2gBU6hKfY`
-						}
-					>
-						<CardMedia
-							image={
-								snippet
-									?.thumbnails
-									?.high
-									?.url ||
-								demoThumbnailUrl
-							}
-							alt={
-								snippet?.title
-							}
-							sx={{
-								width: "200px",
-								height: "146px",
-								borderRadius: "5px",
-							}}
-						/>
-					</Link>
-				</Stack>
-			</Grid>
-			<Grid item xs={6} sm={6}>
-				<Stack>
-					<CardContent
-						sx={{
-							backgroundColor:
-								"#FFF",
-							height: "106px",
-						}}
-					>
+}) => {
+	// Check if snippet is available
+	const timestamp1 = snippet?.publishedAt;
+	const dateObject1 = new Date(timestamp1);
+
+	const timestamp2 = new Date();
+	const dateObject2 = new Date(timestamp2);
+
+	// Menghitung selisih waktu dalam milisekon
+	const timeDifference = dateObject2 - dateObject1;
+
+	// Menghitung selisih waktu dalam hari, bulan, dan tahun
+	const daysDifference = Math.floor(
+		timeDifference / (1000 * 60 * 60 * 24)
+	);
+	const monthsDifference =
+		dateObject2.getMonth() -
+		dateObject1.getMonth() +
+		12 *
+			(dateObject2.getFullYear() -
+				dateObject1.getFullYear());
+	const yearsDifference =
+		dateObject2.getFullYear() - dateObject1.getFullYear();
+
+	// Menentukan label untuk menampilkan
+	let displayLabel;
+	let displayValue;
+
+	if (daysDifference > 30) {
+		displayLabel = "Bulan";
+		displayValue = monthsDifference;
+	} else if (yearsDifference > 1) {
+		displayLabel = "Tahun";
+		displayValue = yearsDifference;
+	} else {
+		displayLabel = "Hari";
+		displayValue = daysDifference;
+	}
+	return (
+		<Card
+			sx={{
+				width: {
+					xs: "100%",
+					sm: "358px",
+					md: "420px",
+				},
+				boxShadow: "none",
+				borderRadius: 2,
+				border: "1px solid gray",
+			}}
+		>
+			<Grid container spacing={2}>
+				<Grid item xs={6} sm={6}>
+					<Stack>
 						<Link
 							to={
 								videoId
 									? `/video/${videoId}`
-									: demoVideoUrl
+									: `/video/cV2gBU6hKfY`
 							}
 						>
-							<Typography
-								variant="subtitle1"
-								fontWeight="bold"
-								color="#000"
+							<CardMedia
+								image={
+									snippet
+										?.thumbnails
+										?.high
+										?.url ||
+									demoThumbnailUrl
+								}
+								alt={
+									snippet?.title
+								}
 								sx={{
-									fontSize: "12px",
-									fontFamily: "'Roboto', sans-serif",
+									width: "200px",
+									height: "146px",
+									borderRadius: "5px",
 								}}
+							/>
+						</Link>
+					</Stack>
+				</Grid>
+				<Grid item xs={6} sm={6}>
+					<Stack>
+						<CardContent
+							sx={{
+								backgroundColor:
+									"#FFF",
+								height: "106px",
+							}}
+						>
+							<Link
+								to={
+									videoId
+										? `/video/${videoId}`
+										: demoVideoUrl
+								}
 							>
-								{snippet?.title.slice(
-									0,
-									60
-								) ||
-									demoVideoTitle.slice(
+								<Typography
+									variant="subtitle1"
+									fontWeight="bold"
+									color="black"
+									sx={{
+										fontSize: "12px",
+										fontFamily: "'Roboto', sans-serif",
+									}}
+								>
+									{snippet?.title.slice(
 										0,
 										60
-									)}
-							</Typography>
-						</Link>
-						<Link
-							to={
-								snippet?.channelId
-									? `/channel/${snippet?.channelId}`
-									: demoChannelUrl
-							}
-						>
+									) ||
+										demoVideoTitle.slice(
+											0,
+											60
+										)}
+								</Typography>
+							</Link>
+							<Link
+								to={
+									snippet?.channelId
+										? `/channel/${snippet?.channelId}`
+										: demoChannelUrl
+								}
+							>
+								<Typography
+									variant="subtitle2"
+									color="gray"
+									sx={{
+										fontSize: "10px",
+										color: "gray",
+										ml: "5px",
+										fontFamily: "'Roboto', sans-serif",
+									}}
+								>
+									{snippet?.channelTitle ||
+										demoChannelTitle}
+									<CheckCircleIcon
+										sx={{
+											fontSize: "8px",
+											color: "gray",
+											ml: "5px",
+										}}
+									/>
+								</Typography>
+							</Link>
 							<Typography
 								variant="subtitle2"
 								color="gray"
@@ -114,22 +177,14 @@ const VideoCard = ({
 									fontFamily: "'Roboto', sans-serif",
 								}}
 							>
-								{snippet?.channelTitle ||
-									demoChannelTitle}
-								<CheckCircleIcon
-									sx={{
-										fontSize: "8px",
-										color: "gray",
-										ml: "5px",
-									}}
-								/>
+								{`${displayValue} ${displayLabel} yang lalu`}
 							</Typography>
-						</Link>
-					</CardContent>
-				</Stack>
+						</CardContent>
+					</Stack>
+				</Grid>
 			</Grid>
-		</Grid>
-	</Card>
-);
+		</Card>
+	);
+};
 
 export default VideoCard;
