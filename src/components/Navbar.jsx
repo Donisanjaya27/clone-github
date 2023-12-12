afcfb30b2f3840a2c41291a2bd780729a1003c86
Doni Sanjaya <donis293@gmail.com>
2023-12-12 22:40:18 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,28 @@ import { logo } from "../utils/constants";
 import { SearchBar } from "./";
 
 const Navbar = () => (
-  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
-    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <img src={logo} alt="logo" height={45} />
-    </Link>
-    <SearchBar />
-  </Stack>
+	<Stack
+		direction="row"
+		alignItems="center"
+		p={2}
+		sx={{
+			position: "sticky",
+			background: "#FFF",
+			top: 0,
+			justifyContent: "space-between",
+		}}
+	>
+		<Link
+			to="/"
+			style={{ display: "flex", alignItems: "center" }}
+		>
+			<img src={logo} alt="logo" height={45} />
+		</Link>
+		<Stack>
+			<SearchBar />
+		</Stack>
+		<Stack></Stack>
+	</Stack>
 );
 
 export default Navbar;
