0fb996131999d60fa468bf1adeaaee86be296ed7
Doni Sanjaya <donis293@gmail.com>
2024-01-10 14:16:36 +0700

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -31,7 +31,7 @@ const SearchBar = () => {
     >
       <input
         className='search-bar'
-        placeholder='Search...'
+        placeholder='Doni sanjaya...'
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
