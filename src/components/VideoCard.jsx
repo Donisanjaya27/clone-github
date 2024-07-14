3921b748cffb3e23c1f6e4021feb67759058b79a
Doni Sanjaya <donis293@gmail.com>
2024-07-14 12:09:17 +0700

    Update VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -6,13 +6,13 @@ import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
 
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
-  <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 0 }}>
+  <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: '50px' }}>
     <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY` }>
       <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title} 
-        sx={{ width: { xs: '100%', sm: '358px'}, height: 180 }} 
+        sx={{ width: { xs: '100%', sm: '358px'}, height: 180, borderTopLeftRadius: '50px', borderTopRightRadius: '50px' }} 
       />
     </Link>
-    <CardContent sx={{ backgroundColor: "#1E1E1E", height: '106px' }}>
+    <CardContent sx={{ backgroundColor: "#000", height: '106px', borderBottomLeftRadius: '50px', borderBottomRightRadius: '50px' }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl } >
         <Typography color="#FFF">
           {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
@@ -28,4 +28,4 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
   </Card>
 );
 
-export default VideoCard
+export default VideoCard;
