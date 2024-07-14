9460a684a2e49c4e139dbb07acb8b07ee8d6981c
Doni Sanjaya <donis293@gmail.com>
2024-07-14 11:00:36 +0700

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -5,8 +5,7 @@ export const BASE_URL = 'https://youtube-v3-lite.p.rapidapi.com';
 const options = {
   params: {
     regionCode: 'ID',
-   maxResults: 2500,
-    part='id,snippet',
+   maxResults: 2500, 
   },
   headers: {
     'X-RapidAPI-Key': 'bf4ad55bb9mshf2105f1de4360a1p1d8d2ejsn7a558d91acac',
