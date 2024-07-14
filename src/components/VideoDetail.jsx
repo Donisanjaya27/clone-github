56dc57c55683bb3c97c58bbc66c103f86663ee61
Doni Sanjaya <donis293@gmail.com>
2024-07-14 12:18:11 +0700

    Update VideoDetail.jsx

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -28,8 +28,8 @@ const VideoDetail = () => {
     <Box minHeight="95vh">
       <Stack direction={{ xs: "column", md: "row" }}>
         <Box flex={1}>
-          <Box sx={{ width: "60%", position: "sticky", top: "86px" }}>
-            <div style={{ borderRadius: '20px', overflow: 'hidden' }}>
+          <Box sx={{ width: "100%", height:"400px" position: "sticky", top: "86px" }}>
+            <div style={{ borderRadius: '10px', overflow: 'hidden' }}>
               <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
             </div>
             <Typography color="#fff" fontWeight="bold" p={2}>
