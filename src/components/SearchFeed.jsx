c3e91dd435b456cba38673e3be3bff43af5f9867
Doni Sanjaya <donis293@gmail.com>
2024-07-14 10:58:03 +0700

    Update SearchFeed.jsx

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -10,14 +10,14 @@ const SearchFeed = () => {
   const { searchTerm } = useParams();
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet,id&q=${searchTerm}`)
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
       .then((data) => setVideos(data.items))
   }, [searchTerm]);
 
   return (
     <Box p={2} minHeight="95vh">
-      <Typography variant="h4" fontWeight={900}  color="#000" mb={3} ml={{ sm: "100px"}}>
-        Search Results for <span style={{ color: "#000" }}>{searchTerm}</span> videos
+      <Typography variant="h6"    color="white" mb={3} ml={{ sm: "100px"}}>
+        Search Results for <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
       </Typography>
       <Box display="flex">
         <Box sx={{ mr: { sm: '100px' } }}/>
