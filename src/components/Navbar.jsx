af5c2f9950129d562d0ef51d368419d4253ccdec
Doni Sanjaya <donis293@gmail.com>
2024-07-14 12:13:26 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { SearchBar } from "./";
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <YouTubeIcon style={{ color: 'red', fontSize: 100 }} />
+      <YouTubeIcon style={{ color: 'red', fontSize: 50 }} />
     </Link>
     <SearchBar />
   </Stack>
