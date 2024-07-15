b24e86aa60aaf2f48e8474b0dd6927596725a4de
Doni Sanjaya <donis293@gmail.com>
2024-07-15 14:28:21 +0700

    Updatee SearchFeed.jsx

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -7,30 +7,33 @@ import { Videos } from "./";
 
 const SearchFeed = () => {
   const [videos, setVideos] = useState(null);
-  const [chanel, setChanel] = useState(null);
+  const [channel, setChannel] = useState(null); // Corrected the spelling
   const [count, setCount] = useState(null);
   const [date, setDate] = useState(null);
   const { searchTerm } = useParams();
 
   useEffect(() => {
-    fetchFromAPI(search?part=snippet&q=${searchTerm})
-      .then((data) => setVideos(data.items))
-    fetchFromAPI(search?part=snippet&type=channel'&q=${searchTerm})
-      .then((data) => setChanel( data.items))
-    fetchFromAPI(search?part=snippet&order=viewCount&q=${searchTerm})
-      .then((data) => setCount( data.items))
-    fetchFromAPI(search?part=snippet&order=date&q=${searchTerm})
-      .then((data) => setDate( data.items))
-
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data.items));
+      
+    fetchFromAPI(`search?part=snippet&type=channel&q=${searchTerm}`)
+      .then((data) => setChannel(data.items)); // Corrected the spelling
+      
+    fetchFromAPI(`search?part=snippet&order=viewCount&q=${searchTerm}`)
+      .then((data) => setCount(data.items));
+      
+    fetchFromAPI(`search?part=snippet&order=date&q=${searchTerm}`)
+      .then((data) => setDate(data.items));
   }, [searchTerm]);
+
   return (
     <Box p={2} minHeight="95vh">
-      <Typography variant="h6"    color="white" mb={3} ml={{ sm: "100px"}}>
-        Hasil Pencarian for <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
+      <Typography variant="h6" color="white" mb={3} ml={{ sm: "100px" }}>
+        Hasil Pencarian untuk <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
       </Typography>
       <Box display="flex">
         <Box sx={{ mr: { sm: '100px' } }}/>
-        {<Videos videos={chanel} />}
+        {<Videos videos={channel} />}
       </Box>
       <Box display="flex">
         <Box sx={{ mr: { sm: '100px' } }}/>
