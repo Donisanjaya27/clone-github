0383fd3f3dff1baf5bba34179e3ab1f3ab43fc73
Doni Sanjaya <donis293@gmail.com>
2024-07-15 09:47:55 +0700

    Update VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -9,10 +9,10 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
   <Card sx={{ width: { xs: '100%', sm: '258px', md: "220px", }, backgroundColor: "#0F151B", boxShadow: "none", borderRadius: '10px' }}>
     <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY` }>
       <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title} 
-        sx={{ width: { xs: '100%', sm: '258px'}, height: 180, borderTopLeftRadius: '10px', borderTopRightRadius: '10px' }} 
+        sx={{ width: { xs: '100%', sm: '258px'}, height: 150, borderTopLeftRadius: '10px', borderTopRightRadius: '10px' }} 
       />
     </Link>
-    <CardContent sx={{ backgroundColor: "#000", height: '106px', borderBottomLeftRadius: '10px', borderBottomRightRadius: '10px' }}>
+    <CardContent sx={{ backgroundColor: "#0F151B", height: '106px', borderBottomLeftRadius: '10px', borderBottomRightRadius: '10px' }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl } >
         <Typography color="#FFF">
           {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
