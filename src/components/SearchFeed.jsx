e3e3361b1e6006e6818537a3753cbf8535ac78dd
Doni Sanjaya <donis293@gmail.com>
2024-07-15 13:28:11 +0700

    Update SearchFeed.jsx

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -6,33 +6,22 @@ import { fetchFromAPI } from "../utils/fetchFromAPI";
 import { Videos } from "./";
 
 const SearchFeed = () => {
-  const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState(null);
+  const [chanel, setChanel] = useState(null);
+  const [count, setCount] = useState(null);
+  const [date, setDate] = useState(null);
   const { searchTerm } = useParams();
 
   useEffect(() => {
-    const fetchData = async () => {
-      let allVideos = [];
+    fetchFromAPI(search?part=snippet&q=${searchTerm})
+      .then((data) => setVideos(data.items))
+    fetchFromAPI(search?part=snippet&type=channel'&q=${searchTerm})
+      .then((data) => setChanel( data.items))
+    fetchFromAPI(search?part=snippet&order=viewCount&q=${searchTerm})
+      .then((data) => setCount( data.items))
+    fetchFromAPI(search?part=snippet&order=date&q=${searchTerm})
+      .then((data) => setDate( data.items))
 
-      const response1 = await fetchFromAPI(`search?part=snippet&q=${searchTerm}`);
-      const data1 = await response1.json();
-      allVideos = [...allVideos, ...data1.items];
-
-      const response2 = await fetchFromAPI(`search?part=snippet&type=channel&q=${searchTerm}`);
-      const data2 = await response2.json();
-      allVideos = [...allVideos, ...data2.items];
-
-      const response3 = await fetchFromAPI(`search?part=snippet&order=viewCount&q=${searchTerm}`);
-      const data3 = await response3.json();
-      allVideos = [...allVideos, ...data3.items];
-
-      const response4 = await fetchFromAPI(`search?part=snippet&order=date&q=${searchTerm}`);
-      const data4 = await response4.json();
-      allVideos = [...allVideos, ...data4.items];
-
-      setVideos(allVideos);
-    };
-
-    fetchData();
   }, [searchTerm]);
   return (
     <Box p={2} minHeight="95vh">
@@ -41,7 +30,19 @@ const SearchFeed = () => {
       </Typography>
       <Box display="flex">
         <Box sx={{ mr: { sm: '100px' } }}/>
+        {<Videos videos={chanel} />}
+      </Box>
+      <Box display="flex">
+        <Box sx={{ mr: { sm: '100px' } }}/>
         {<Videos videos={videos} />}
+      </Box>
+      <Box display="flex">
+        <Box sx={{ mr: { sm: '100px' } }}/>
+        {<Videos videos={count} />}
+      </Box>
+      <Box display="flex">
+        <Box sx={{ mr: { sm: '100px' } }}/>
+        {<Videos videos={date} />}
       </Box>
     </Box>
   );
