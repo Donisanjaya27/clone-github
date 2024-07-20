12e4ed8519be527ea13955bf7528df48b3480133
Doni Sanjaya <donis293@gmail.com>
2024-07-20 18:11:30 +0700

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,16 +1,17 @@
 import axios from 'axios';
 
-export const BASE_URL = 'https://youtube-v3-lite.p.rapidapi.com';
+export const BASE_URL = 'https://www.googleapis.com/youtube/v3/search;
+//export const BASE_URL = 'https://youtube-v3-lite.p.rapidapi.com';
 
 const options = {
   params: {
     regionCode: 'ID',
-   maxResults: 2500, 
+   maxResults: 2500,
+    key: 'AIzaSyBfr4i_TPRSadpPXYe4SHcW0Y5fUnyl7so',
+    type: 'any',
+    part:'snippet'
   },
-  headers: {
-    'X-RapidAPI-Key': 'bf4ad55bb9mshf2105f1de4360a1p1d8d2ejsn7a558d91acac',
-    'X-RapidAPI-Host': 'youtube-v3-lite.p.rapidapi.com',
-  },
+ 
 };
 
 export const fetchFromAPI = async (url) => {
