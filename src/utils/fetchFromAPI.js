07e1135beac9d8d42d34f9a5e7c38a95622d20eb
Doni Sanjaya <donis293@gmail.com>
2024-07-20 18:51:23 +0700

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,19 +1,37 @@
 import axios from 'axios';
 
-export const BASE_URL = 'https://www.googleapis.com';
-//export const BASE_URL = 'https://youtube-v3-lite.p.rapidapi.com';
-
+//export const BASE_URL = 'https://www.googleapis.com';
+export const BASE_URL = 'https://youtube-v3-lite.p.rapidapi.com';
 const options = {
-  params: {
-    regionCode: 'ID',
-   maxResults: 2500,
-    key: 'AIzaSyBfr4i_TPRSadpPXYe4SHcW0Y5fUnyl7so',
-    type: 'any',
+   params: {
+  maxResults: 2500, 
   },
- 
+  headers: {
+    'X-RapidAPI-Key': 'bf4ad55bb9mshf2105f1de4360a1p1d8d2ejsn7a558d91acac',
+    'X-RapidAPI-Host': 'youtube-v3-lite.p.rapidapi.com',
+  },
+
 };
 
 export const fetchFromAPI = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
   return data;
 };
+
+
+    
+
+// const options = {
+//   params: {
+//     regionCode: 'ID',
+//    maxResults: 2500,
+//     key: 'AIzaSyBfr4i_TPRSadpPXYe4SHcW0Y5fUnyl7so',
+//     type: 'any',
+//   },
+ 
+// };
+
+// export const fetchFromAPI = async (url) => {
+//   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+//   return data;
+// };
