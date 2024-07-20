ff45a885506f50bad3aa0387bbf872c37e5231c3
Doni Sanjaya <donis293@gmail.com>
2024-07-20 18:35:50 +0700

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'https://www.googleapis.com;
+export const BASE_URL = 'https://www.googleapis.com';
 //export const BASE_URL = 'https://youtube-v3-lite.p.rapidapi.com';
 
 const options = {
